0d72a946d6b85230f122a820c6d6f09274d92fba
arfni60 <arfni60@yahoo.com>
2025-01-03 21:58:11 +0100

    score ui
    - added canvas with text score
    - added scoring system to sripts

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    public int score;
+    public int score = 0;
+    public Text scoreText;
+
+    private void Start() 
+    {
+        UpdateScoreUI();
+    }
+    
 
     public void AddToScore(int amount)
     {
         score += amount;
+        UpdateScoreUI();
+    }
+
+    void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + score;
     }
 }
